0b6c8de106f85e81ff5f968d50671eb622280d0d
Blake La Pierre <blakelapierre@gmail.com>
2018-02-13 19:03:33 -0500

    old test work

--- src/app.js
+++ src/app.js
@@ -1,139 +1,88 @@
 import { h, render } from 'preact-cycle';
 
-const ADD_TRACKER_ITEM = ({
-  // jshint ignore:start
-  tracker: {
-    items,
-    inputText,
-    ...trackerProps
-  }, ...props
-}) => ({
-  tracker: {
-    items: items.concat(inputText),
-    inputText: '',
-    ...trackerProps
-  }, ...props
-  // jshint ignore:end
-});
+import annyang from 'annyang';
 
-const SET_TRACKER_TEXT = ({
-  // jshint ignore:start
-  tracker: {
-    inputText,
-    ...trackerProps
-  },
-  ...props
-}, event) => ({
-  tracker: {
-    inputText: event.target.value,
-    ...trackerProps
-  },
-  ...props
-  // jshint ignore:end
-});
+let inited = false;
+
+function init(mutation) {
+  inited = true;
+
+  const rootCommands = {
+    'test': () => alert('test!'),
+    'new task': (...args) => {
+      respond('New Task');
+      mutation(NEW_TASK)(...args);
+      annyang.removeCommands(rootCommands);
+      annyang.addCommands(newTaskCommands);
+    }
+  };
+
+  const newTaskCommands = {
+    '*other': (...args) => {
+      respond(args.join(' '));
+      mutation(NEW_TASK_TEXT)(...args);
+      annyang.removeCommands(newTaskCommands);
+      annyang.addCommands(rootCommands);
+    }
+  };
+
+  annyang.addCommands(rootCommands);
+
+  annyang.start();
+}
+
+const NEW_TASK = _ => {
+  if (_.tracker.items.length > 0) {
+    _.tracker.items[0].end = new Date().getTime();
+  }
+
+  _.tracker.items.unshift(new Task(''));
+  return _;
+};
+
+class TObject {
+  constructor() {
+    this.time = new Date().getTime();
+  }
+}
+
+class Task extends TObject {
+  constructor(text) {
+    super();
+    this.text = text;
+  }
+}
+
+const NEW_TASK_TEXT = (_, text) => {
+  _.tracker.items[0].text = text;
+  return _;
+};
 
 const fromEvent = (prev, event) => event.target.value;
 
-const Tracker = ({tracker:{items, inputText}}, {mutation}) => (
+
+const Tracker = ({tracker: {items}}, {mutation}) => (
   // jshint ignore:start
   <tracker>
-    {items.map(item => <item>{item}</item>)}
-    <TrackerInput inputText={inputText} />
+    {!inited ? init(mutation) : undefined}
+    {items.map(item => <item>{item.text} {new Date(item.time).toString()} {item.end ? new Date(item.end).toString() : undefined} {item.end ? (item.end - item.time) / 1000 + 's' : undefined}</item>)}
   </tracker>
-  // jshint ignore:end
-);
-
-const TrackerInput = ({inputText}, {mutation}) => (
-  // jshint ignore:start
-  <tracker-input>
-    <form onSubmit={mutation(ADD_TRACKER_ITEM)} action="javascript:">
-      <input placeholder="New item..." value={inputText} onInput={mutation(SET_TRACKER_TEXT)} autoFocus />
-    </form>
-  </tracker-input>
-  // jshint ignore:end
-);
-
-const Info = ({items}, {info: {metrics}}) => (
-  // jshint ignore:start
-  <info>
-    <headers>
-      {metrics.map(metric => <Metric metric={metric} />)}
-    </headers>
-    <bars>
-      {metrics.map(metric => <Bar value={Math.random() * 100} />)}
-    </bars>
-  </info>
-  // jshint ignore:end
-);
-
-const Metric = ({metric: {name, units}}) => (
-  // jshint ignore:start
-  <metric>{name} ({units[0]})</metric>
-  // jshint ignore:end
-);
-
-const Bar = ({value}) => (
-  // jshint ignore:start
-  <bar style={{'height': `${value}%`}}>bar</bar>
-  // jshint ignore:end
-);
-
-const SideBySide = ({tracker, info}) => (
-  // jshint ignore:start
-  <side-by-side>
-    <Tracker tracker={tracker} />
-    <Info info={info} />
-  </side-by-side>
   // jshint ignore:end
 );
 
 render(
   // jshint ignore:start
-  SideBySide, {
-    tracker: {items: [], text: ''},
-    info: {
+  Tracker, {
+    tracker: {
       items: [],
-      metrics: [{
-        name: 'Calories',
-        units: ['kcal']
-      },{
-        name: 'Saturated Fat',
-        units: ['g'],
-        group: 'Total Fat'
-      },{
-        name: 'Trans Fat',
-        units: ['g']
-      },{
-        name: 'Monounsaturated Fat',
-        units: ['g'],
-        group: 'Unsaturated Fat'
-      },{
-        name: 'Polyunsaturated Fat',
-        units: ['g'],
-        group: 'Unsaturated Fat'
-      },{
-        name: 'Sugars',
-        units: ['g']
-      },{
-        name: 'Soluble Fiber',
-        units: ['g']
-      },{
-        name: 'Insoluble Fiber',
-        units: ['g']
-      },{
-        name: 'Other Carbohydrates',
-        units: ['g']
-      },{
-        name: 'Protein',
-        units: ['g']
-      },{
-        name: 'Sodium',
-        units: ['mg']
-      },{
-        name: 'Potassium',
-        units: ['mg']
-      }]
-    },
+      inputText: ''
+    }
   }, document.body
   // jshint ignore:end
 );
+
+function respond(...args) {
+  const utterance = new SpeechSynthesisUtterance(...args);
+  window.speechSynthesis.speak(utterance);
+  console.log('Responded: ', ...args);
+}
